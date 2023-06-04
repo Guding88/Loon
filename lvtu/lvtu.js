eb0dd1e43b8cf8002196073f36971ad0f9be7fbd
Guding88 <132737252+Guding88@users.noreply.github.com>
2023-06-04 19:46:29 +0800

    Create lvtu.js

---
+++ lvtu/lvtu.js
@@ -0,0 +1,27 @@
+let body = {
+  "success" : true,
+  "message" : "success",
+  "state" : 200,
+  "content" : {
+    "headUrl" : "https:\/\/www.1314zhilv.com\/resources\/lxwalkman\/userLogo.png",
+    "isMountSwitch" : 0,
+    "isAnchor" : 1,
+    "isSYGuidePopUp" : 1,
+    "isShowAppletWX" : 0,
+    "isDistributor" : 0,
+    "isShowGrey" : 0,
+    "levDesc" : "尊贵的破解会员",
+    "totalCashBalance" : 0,
+    "userName" : "Test",
+    "userId" : "666",
+    "phoneNum" : "666",
+    "identityType" : 1,
+    "cashBalance" : 0,
+    "isShowBroadcast" : 0,
+    "isShowShareBtn" : 0,
+    "isShowAppletDY" : 0,
+    "isActivate" : 1
+  }
+}
+
+$done({ body: JSON.stringify(body) });
