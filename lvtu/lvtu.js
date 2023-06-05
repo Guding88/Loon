ca2b165fe71fc49f2bd472d428210621a8123100
Guding88 <132737252+Guding88@users.noreply.github.com>
2023-06-05 15:37:47 +0800

    Create lvtu.js

---
+++ lvtu/lvtu.js
@@ -0,0 +1,27 @@
+let body = {
+  "success" : true,
+  "message" : "success",
+  "state" : 200,
+  "content" : {
+    "headUrl" : null,
+    "isMountSwitch" : 0,
+    "isAnchor" : 1,
+    "isSYGuidePopUp" : 1,
+    "isShowAppletWX" : 0,
+    "isDistributor" : 0,
+    "isShowGrey" : 0,
+    "levDesc" : "无耻的破解会员",
+    "totalCashBalance" : 0,
+    "userName" : "仅供测试使用",
+    "userId" : "1",
+    "phoneNum" : "",
+    "identityType" : 1,
+    "cashBalance" : 0,
+    "isShowBroadcast" : 0,
+    "isShowShareBtn" : 0,
+    "isShowAppletDY" : 0,
+    "isActivate" : 1
+  }
+}
+
+$done({ body: JSON.stringify(body) });
