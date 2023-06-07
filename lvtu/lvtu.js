50089f885eb214d06ad0851e0e5fad3cb63932ac
Guding <132737252+Guding88@users.noreply.github.com>
2023-06-07 15:53:48 +0800

    Update and rename lvtutest.js to lvtu.js

--- lvtu/lvtu.js
+++ lvtu/lvtu.js
@@ -5,5 +5,9 @@ guding.content["userId"] = "1";
 guding.content["identityType"] = 1;
 guding.content["headUrl"] = "https://raw.githubusercontent.com/Guding88/iCon/main/Guding.png";
 guding.content["isActivate"] = 1;
+guding.content["levDesc"] = "无耻的破解会员";
+guding.contend["userName"] = "骨钉";
+
 
 $done({ body: JSON.stringify(guding) });
+console.log($response.body);
