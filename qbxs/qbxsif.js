45f3bec7a2db790ca665200e964dc7e81130d2ac
Guding <132737252+Guding88@users.noreply.github.com>
2023-06-07 17:38:09 +0800

    Update qbxsif.js

--- qbxs/qbxsif.js
+++ qbxs/qbxsif.js
@@ -1,6 +1,32 @@
-try {
-  const jsonData = JSON.parse($response.body);
-  console.log("Valid JSON");
-} catch (error) {
-  console.log("Invalid JSON");
+let responseBody = $response.body;
+
+// 将非字符串类型的 responseBody 转换为字符串
+if (typeof responseBody !== "string") {
+  responseBody = JSON.stringify(responseBody);
 }
+
+// 修改字符串内容
+responseBody = {
+  "errcode": 0,
+  "errmsg": "success",
+  "data": {
+    "uid": 127539345,
+    "bookCoins": 555,
+    "rechargeCoins": 555,
+    "rechargeCount": 555,
+    "vipRechargeCount": 5,
+    "vipRechargeMoney": 5,
+    "consumeCoins": 5,
+    "consumeCount": 5,
+    "bdConsumeCount": 5,
+    "paymentUser": true,
+    "freeUser": 0,
+    "createdOn": 1686126799000,
+    "modifiedOn": 1686126799000,
+    "isVip": true
+  }
+};
+
+let modifiedBody = JSON.stringify(responseBody);
+
+$done({ body: modifiedBody });
