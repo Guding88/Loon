7234898cd7d5e22f178271095fcc790ef878affc
Guding <132737252+Guding88@users.noreply.github.com>
2023-06-07 15:40:23 +0800

    Update lvtutest.js

--- lvtu/lvtutest.js
+++ lvtu/lvtutest.js
@@ -2,8 +2,8 @@ var guding = JSON.parse($response.body);
 console.log($response.body);
 
 guding.data["userId"] = "1";
-guding.data["identityType"] = "1";
+guding.data["identityType"] = 1;
 guding.data["headUrl"] = "https://raw.githubusercontent.com/Guding88/iCon/main/Guding.png";
-guding.data["isActivate"] = "1";
+guding.data["isActivate"] = 1;
 
 $done({ body: JSON.stringify(guding) });
