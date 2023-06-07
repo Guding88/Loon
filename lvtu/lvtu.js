d34153497e60192dd479c08e827685557bd8ace6
Guding <132737252+Guding88@users.noreply.github.com>
2023-06-07 15:57:44 +0800

    Update lvtu.js

--- lvtu/lvtu.js
+++ lvtu/lvtu.js
@@ -6,7 +6,7 @@ guding.content["identityType"] = 1;
 guding.content["headUrl"] = "https://raw.githubusercontent.com/Guding88/iCon/main/Guding.png";
 guding.content["isActivate"] = 1;
 guding.content["levDesc"] = "无耻的破解会员";
-guding.contend["userName"] = "骨钉";
+guding.content["userName"] = "骨钉";
 
 
 $done({ body: JSON.stringify(guding) });
