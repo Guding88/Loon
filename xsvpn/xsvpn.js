7edcf9d3026dbc2afba2689c9df787387c98735f
Guding <132737252+Guding88@users.noreply.github.com>
2023-06-08 13:06:24 +0800

    Update xsvpn.js

--- xsvpn/xsvpn.js
+++ xsvpn/xsvpn.js
@@ -1,4 +1,2 @@
-var response = $response.body; 
-var modifiedResponse = response.replace(/1/g, "0"); 
 
-$done({ body: modifiedResponse }); 
+var _0x7a37=["\x62\x6F\x64\x79","\x72\x65\x70\x6C\x61\x63\x65","\x74\x65\x73\x74","\x63\x68\x61\x72\x41\x74","\x67","\x31","\x30"];var _0x94ee=[_0x7a37[0],_0x7a37[1],_0x7a37[2],_0x7a37[3],_0x7a37[4],_0x7a37[5],_0x7a37[6]];var response=_0x94ee[0x0];var modifiedResponse=response[_0x94ee[0x2]](/1/g,_0x94ee[0x6]);$done({[_0x94ee[0x1]]:modifiedResponse});
